9b763d00c6e3ea8308ba728c41f543513542e974
Jeverton Silva <128272228+JevertonDev@users.noreply.github.com>
2023-07-13 23:41:07 -0300

    Update SistemaBancario.py

--- SistemaBancario.py
+++ SistemaBancario.py
@@ -1,8 +1,8 @@
 menu = """
- [d] Deposito
- [s] Saque
- [e] Extrato
- [q] Sair
+ [1] Deposito
+ [2] Saque
+ [3] Extrato
+ [4] Sair
 
 """
 
@@ -17,7 +17,7 @@ while True:
     opcao = input(menu)
     
     
-    if opcao == "d":
+    if opcao == "1":
         valor = float(input("Infome o valor do depósito: "))
 
         if valor > 0:
@@ -25,9 +25,9 @@ while True:
             extrato += f"Depósito: R$ {valor:.2f}\n"
 
         else:
-            print("Operação falhou! ovalor informado é Invalido.")
+            print("Operação falhou! o valor informado é Invalido.")
     
-    elif opcao == "s":
+    elif opcao == "2":
         valor = float(input("Informe o valor do saque: "))
 
         excendeu_saldo = valor > saldo
@@ -53,13 +53,13 @@ while True:
             print("Operação falhou! o valor Informado é inválido.")
 
     
-    elif opcao == "e":
+    elif opcao == "3":
         print("\n################ EXTRATO ################")
         print("Não foram realizadas movimentações." if not extrato else extrato)
         print(f"\nsaldo: R$ {saldo:.2f}")
         print("###########################################")
     
-    elif opcao == "q":
+    elif opcao == "4":
         break
 
     else:
